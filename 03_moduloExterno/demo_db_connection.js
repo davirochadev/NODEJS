a82594f78a102b74778097f54600e422be7dc186
davirochadev <davi.rochab@gmail.com>
2022-11-08 22:26:46 -0300

    Utilizando modulo mysql2 para conexão com base de dados

--- 03_moduloExterno/demo_db_connection.js
+++ 03_moduloExterno/demo_db_connection.js
@@ -1,13 +1,20 @@
-var mysql = require('mysql');
+var mysql = require('mysql2');
 
 var con = mysql.createConnection({
-  host: "127.0.0.1",
+  host: "localhost",
   port: "3306",
   user: "root",
-  password: "D@vi1303"
+  password: "D@vi13032014",
+  database: "db_senai"
 });
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
+});
+
+con.query('SELECT * FROM ANIMAL', (erro, linhas) => {
+    if (erro) throw erro;
+
+    console.log('Animais: ', linhas, '\n\n')
 });
