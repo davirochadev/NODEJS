b5fb0c6061c59288aacfbdaaa8811936a24c94cc
davirochadev <davi.rochab@gmail.com>
2022-11-08 21:05:23 -0300

    Implementando módulo de operações matemáticas

---
+++ 01_moduloInterno/index.js
@@ -0,0 +1,14 @@
+const meuModulo = require('./meuModulo');
+
+const soma = meuModulo.soma;
+const subtracao = meuModulo.subtracao;
+const divisao = meuModulo.divisao;
+const multiplicacao = meuModulo.multiplicacao;
+
+numero1 = 10;
+numero2 = 5;
+
+console.log(soma(numero1, numero2));
+console.log(subtracao(numero1, numero2));
+console.log(divisao(numero1, numero2));
+console.log(multiplicacao(numero1, numero2));
