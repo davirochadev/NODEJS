640beaf903de4ea5f074265adf90a32287c46b78
davirochadev <davi.rochab@gmail.com>
2022-11-09 21:49:30 -0300

    aplicando um exemplo de middleware

--- 04_express/index.js
+++ 04_express/index.js
@@ -7,6 +7,23 @@ const port = 3000; // porta de acesso ao servidor
 
 const basePath = path.join(__dirname, 'templates');
 
+
+const checarAutenticacao = function (req, res, next) {
+    req.authStatus = true;
+
+    if(req.authStatus) {
+        console.log('Usuário Logado');
+        next()
+    }
+    else {
+        console.log('Usuário não está logado. Favor fazer o login');
+        next();
+    }
+
+}
+
+app.use(checarAutenticacao);
+
 app.get('/', (requisicao, resposta) => {
     resposta.sendFile(`${basePath}/index.html`);
 });
