883d6e210c55cc6ef4165f6b9ae8b9081682cba4
davirochadev <davi.rochab@gmail.com>
2022-11-09 21:16:54 -0300

    adicionando renderização de hmtl usando express

--- 04_express/index.js
+++ 04_express/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 const port = 3000; // porta de acesso ao servidor
 
+const basePath = path.join(__dirname, 'templates');
+
 app.get('/', (requisicao, resposta) => {
-    resposta.send('Esta é minha primeira página utilizando o Node.js');
+    resposta.sendFile(`${basePath}/index.html`);
 });
 
 app.listen(port, () => {
